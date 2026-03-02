514e32de646c4490c484c40d8209c1f1962e9932
dnsdcz <delacruz_denise@plpasig.edu.ph>
2026-03-02 22:01:45 +0800

    Exercise 4 - Views and Controller

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\View;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -27,6 +28,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::share('sharedVariable', 'This is a shared variable');
     }
 }

8b2d16997de6c9500d267ee339bd605b91d8fe78
dnsdcz <delacruz_denise@plpasig.edu.ph>
2026-03-20 02:19:45 +0800

    Exercise #5 - Blade Templates

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,38 +1,25 @@
-<h2>Products</h2>
+<x-layout>
+    <x-slot:heading>
+        Product List
+    </x-slot:heading>
 
-<table border="1" cellpadding="8" cellspacing="0">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Category</th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse ($products as $product)
+    <x-table>
+        <thead>
             <tr>
-                <td>{{ $product['id'] }}</td>
-                <td>{{ $product['name'] }}</td>
-                <td>{{ $product['category'] }}</td>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Category</th>
             </tr>
-        @empty
-            <tr>
-                <td colspan="3">No products found.</td>
-            </tr>
-        @endforelse
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach ($products as $product)
+                <tr>
+                    <td>{{ $product['id'] }}</td>
+                    <td>{{ $product['name'] }}</td>
+                    <td>{{ $product['category'] }}</td>
+                </tr>
+            @endforeach
+        </tbody>
 
-<h1>Tasks</h1>
-<ul>
-    @foreach ($tasks as $task)
-        <li>{{ $task }}</li>
-    @endforeach
-</ul>
-
-
-<p>Golbal Variables:</p>
-<p>{{$sharedVariable}}</p>
-
-<p>Product Key:</p>
-<p>{{$productKey}}</p>
+    </x-table>
+</x-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,21 @@
-<html>
-    <body>
-        <h1>Welcome to {{ $name }} </h1>
-</body>
-</html>
+<x-layout>
+    <x-slot name="heading">
+        WELCOME TO DELACRUZ APP
+    </x-slot>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="jumbotron">
+                    <h1 class="display-4">Welcome</h1>
+                    <p class="lead">This is your application welcome page.</p>
+                    <hr class="my-4">
+                    <p>Use the navigation bar to explore the app.
+
+                        <a class="btn btn-primary btn-lg" href="/products" role="button">View Products</a>
+                     </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
